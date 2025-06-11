133f95fc7f1732cdfe581dee5f76c7df918c5a12
impelfin <impelfin@gmail.com>
2025-06-12 08:42:53 +0900

    up

--- xavier/installTorch.sh
+++ xavier/installTorch.sh
@@ -4,7 +4,9 @@ echo "==================================="
 echo " # install the dependencies (if not already onboard)"
 echo "==================================="
 
+sudo apt install nvidia-jetpack
 sudo apt-get install -y python3-pip libjpeg-dev libopenblas-dev libopenmpi-dev libomp-dev
+sudo pip3 install -U jetson-stats
 sudo -H pip3 install future
 sudo pip3 install -U --user wheel mock pillow
 sudo -H pip3 install testresources
