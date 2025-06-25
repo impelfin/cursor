6c6c4894ef6baf8a398f52e138a7d470c7283282
JunHyeon Mun <impelfin@gmail.com>
2025-06-25 17:02:05 +0900

    ddd

--- dify/api/models/base.py
+++
@@ -1,7 +0,0 @@
-from sqlalchemy.orm import DeclarativeBase
-
-from models.engine import metadata
-
-
-class Base(DeclarativeBase):
-    metadata = metadata
--- dify/web/app/components/workflow-app/hooks/index.ts
+++
@@ -1,7 +0,0 @@
-export * from './use-workflow-init'
-export * from './use-workflow-template'
-export * from './use-nodes-sync-draft'
-export * from './use-workflow-run'
-export * from './use-workflow-start-run'
-export * from './use-is-chat-mode'
-export * from './use-workflow-refresh-draft'
